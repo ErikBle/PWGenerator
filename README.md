b50db98c13e2103e09e0de762bb997b1922dca8b
Yfopla <ekblecker@gmail.com>
2020-02-07 20:40:31 -0500

    Completed code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PWGenerator
+Erik's TOTALLY AMAZING low-fuss Password Generator!
+
+Are you tired of Password Generators with fancy graphics and Matrix-like UI? I'm not.
+
+But if you are, then try this Password generator! Put in the number of characters you want your
+password to be, put in if you want cool unhackable* things like Numbers or Special Characters,
+then like magic, your toally random password is right there, before your very eyes!
+
+
+
+*not guaranteed
--- script.js
+++ script.js
@@ -12,3 +12,57 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Added variables 
+var allLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+var upperLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var upperNumLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var upperCharLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*';
+var numLetters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var charLetters = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*';
+var numCharLetters = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*'
+var noneLetters ='abcdefghijklmnopqrstuvwxyz';
+var result = '';
+
+
+function generatePassword() {
+
+  // variables needed for this function
+  var pwLength = parseInt(prompt("Input how many characters you would like your password to be (between 8 and 128)"));
+  var confirmUC = confirm("Would you like Uppercase letters?")
+  var confirmNum = confirm("Would you like numbers?")
+  var confirmSpec = confirm("Would you like special characters?")
+
+
+  for (var i=0; i<=pwLength; i++)  {
+   
+    // All possiblities 
+    if (confirmUC === true && confirmNum === true && confirmSpec === true) {
+        result = result + allLetters.charAt(Math.floor(Math.random()*allLetters.length -1)) } 
+    
+    else if(confirmUC === true && confirmNum === true && confirmSpec === false) {
+         result = result + upperNumLetters.charAt(Math.floor(Math.random()*upperNumLetters.length -1)) }
+
+    else if(confirmUC === true && confirmSpec === true && confirmNum === false) {
+        result = result + upperCharLetters.charAt(Math.floor(Math.random()*upperCharLetters.length -1)) }
+    
+    else if(confirmUC === true && confirmNum === false && confirmSpec === false) {
+      result = result + upperLetters.charAt(Math.floor(Math.random()*upperLetters.length -1)) } 
+    
+    else if(confirmNum === true && confirmSpec === true && confirmUC === false) {
+      result = result + numCharLetters.charAt(Math.floor(Math.random()*numCharLetters.length -1)) } 
+    
+    else if(confirmNum === true && confirmSpec === false && confirmUC === false) {
+        result = result + numLetters.charAt(Math.floor(Math.random()*numLetters.length -1)) } 
+
+    else if(confirmSpec === true && confirmNum === false && confirmUC === false) {
+          result = result + charLetters.charAt(Math.floor(Math.random()*charLetters.length -1)) } 
+
+    else  {
+      result = result + noneLetters.charAt(Math.floor(Math.random()*noneLetters.length -1)) } } 
+    
+    // Returns the password result
+    return result;
+
+    
+        }    
